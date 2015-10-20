a95808f2fb208356568504a54e50b0b4a2b9770a
王默 <maker@maker-air.local>
2015-10-20 18:13:38 +0800

    init

--- index.php
+++ index.php
@@ -17,16 +17,7 @@ body{
 	width:100%;
 	height:100%;	
 }
-/*
-#navbar{
-	color:#ffffff;
-	background:#000000;	
-	height:40px;
-	line-height:40px;
-}
-*/
 #main{
-	/*background:blue;*/
 	width:100%;
 }
 #tabs{
@@ -39,7 +30,7 @@ body{
 	width:100%;
 }
 #tabs .file img{
-	width:60%;
+	/*width:60%;*/
 }
 #bottombar{
 	width:100%;
@@ -52,23 +43,26 @@ body{
 	opacity:0.3;
 	filter:alpha(opacity=30); /* 针对 IE8 以及更早的版本 */
 }
+#btn_pause {
+	display:none;
+}
+#btn_reset {
+	display:none;
+}
+#btn_play, #btn_pause, #btn_reset{
+	margin-left:5px;
+	padding: 5px;
+}
 </style>
 </head>
 <body>
 <div id="container">
-<!--
-    <nav id="navbar" class="">
-	autotab.cc
-    </nav>
--->
     <div id="main" class="">
 	<div id="tabs">
 	</div>
     </div>
     <nav id="bottombar" class="">
-	<button id="btn_play">Play</button>
-	<button id="btn_pause">Pause</button>
-	<button id="btn_reset">Reset</button>
+	<button id="btn_play">Play</button><button id="btn_pause">Pause</button><button id="btn_reset">Reset</button>
     </nav>
 </div>
 </body>
@@ -78,19 +72,26 @@ var tabs = [
 	{
 		id:1,
 		name:'贝加尔湖畔',
-		speed:0.75,
+		speed:200, // 这个数值代表多少秒滚动一张曲谱
+		scale:0.6,
 		files:[
 			'./uploads/tabs/1/20150924/1.jpg',
 			'./uploads/tabs/1/20150924/2.jpg',
+			'./uploads/tabs/1/20150926/1.png',
+			'./uploads/tabs/1/20150926/2.png',
+			'./uploads/tabs/1/20150926/3.png'
+		],
+		size:[
+			[2480,3508],
+			[2480,3508]
 		],
 		skips:[
-			[0,0.1],
-			[0.9,1],
-			[1.0,1.1],
-			[1.9,2]
+			[0,0.1], // 第一页页头
+			[0.85,1], // 第一页页尾
+			[0.85,1] // 第一页页尾 重复一次
 		],
 		repeats:[
-			[0.6,0.3],
+			//[0.6,0.3],
 			[1.7,0.7]
 		]
 	},
@@ -98,9 +99,14 @@ var tabs = [
 		id:2,
 		name:'贝加尔湖畔2',
 		speed:1,
+		scale:0.8,
 		files:[
 			'/uploads/tabs/1/20150924/1.jpg',
 			'/uploads/tabs/1/20150924/2.jpg',
+		],
+		size:[
+			[2480,3508],
+			[2480,3508]
 		],
 		skips:[
 		],
@@ -109,26 +115,77 @@ var tabs = [
 	},
 ];
 var timer;
-var current_tab = 0;
-var current_position = 0;
-function tab_scroll() {
-	$('#tabs').css('top', - current_position);
+
+// 窗口尺寸
+var screen_width  = 0;
+var screen_height = 0;
+
+// 曲谱缩放后尺寸
+var tab_width  = 0;
+var tab_height = 0;
+
+// 曲谱每0.1秒滚动距离
+var step = 0;
+
+var current_tab_index = 0;
+var current_tab;
+
+// 当前滚动距离
+var current_scroll = 0;
+// 当前滚动事件
+var current_time = 0;
+function clone(obj){
+	var o;
+	if(typeof obj == "object"){
+		if(obj === null){
+			o = null;
+		}else{
+			if(obj instanceof Array){
+				o = [];
+				for(var i = 0, len = obj.length; i < len; i++){
+					o.push(clone(obj[i]));
+				}
+			}else{
+				o = {};
+				for(var k in obj){
+					o[k] = clone(obj[k]);
+				}
+			}
+		}
+	}else{
+		o = obj;
+	}
+	return o;
+}
+function tab_scroll(offset) {
+	//current_scroll += offset;
+	//console.log(current_scroll);
+	//window.scrollTo(0, current_scroll);
+	position = get_real_position_by_time();
+
+	scroll = position * tab_height - (tab_height * 0.3);
+	if (scroll < 0)
+		scroll = 0;
+	//console.log(position);
+	window.scrollTo(0, scroll);
 }
 function tab_reset() {
-	current_position = 0;
-	tab_scroll();
+	//current_position = $(window).scrollTop();
+	//tab_scroll(-current_position);
 }
 function tab_play() {
-
 	timer = setInterval(function(){
-		current_position += tabs[current_tab].speed;
-		tab_scroll();
+		tab_scroll(step);
+		current_time += 0.1;
 	},100);
 }
 function tab_pause() {
 	clearInterval(timer);
 }
 function tab_init(tab) {
+
+	current_tab = tab;
+
 	html = '';
 	for (i in tab.files) {
 		html += '<div class="file">';
@@ -136,18 +193,79 @@ function tab_init(tab) {
 		html += '</div>';
 	}
 	$('#tabs').html(html);
+
+	$('#tabs .file img').css('width', (tab.scale * 100) + '%');
+
+	// todo: why doesn't work
+	//window.scrollTo(0, 0);	
 }
+// 取得最优缩放比例
+function get_scale(screen_width, screen_height, tab_width,  tab_height) {
+	if (screen_width > screen_height)
+		return (screen_height/0.6) / tab_height;
+	else
+		return tab_width / screen_width;
+}
+
+function get_position_by_time() {
+	position = current_time / current_tab.speed;
+	return position;
+}
+
+function get_real_position_by_time() {
+	ct = clone(current_tab);
+
+	position = current_time / current_tab.speed;
+
+	real_position = position;
+
+	// 跳过无效区域
+	for (i in ct.skips) {
+		if ((ct.skips[i][0] < real_position) && ((typeof ct.skips[i][2]) == 'undefined')) {
+			real_position += ct.skips[i][1] - ct.skips[i][0];
+			ct.skips[i][2] = 1;
+		}
+	}
+
+	// 计算重复区域
+	for (i in ct.repeats) {
+		if ((ct.repeats[i][0] < real_position) && ((typeof ct.repeats[i][2]) == 'undefined')) {
+			real_position -= ct.repeats[i][0] - ct.repeats[i][1];
+			ct.repeats[i][2] = 1;
+		}
+	}
+
+	console.log(position + " " + real_position);
+
+	return real_position;
+}
+
 function bind_event() {
 	$('#btn_play').click(function(){
 		tab_play();
+		$('#btn_pause').show();
+		$('#btn_play').hide();
 	});
 
 	$('#btn_pause').click(function(){
 		tab_pause();
+		$('#btn_play').show();
+		$('#btn_pause').hide();
 	});
 
 	$('#btn_reset').click(function(){
 		tab_reset();
+	});
+
+	$(window).scroll(function(){
+		if (timer) {
+			current_position = $(window).scrollTop();
+			if (current_position < (current_scroll - 1)) {
+				tab_pause();
+				$('#btn_play').show();
+				$('#btn_pause').hide();
+			}
+		}
 	});
 }
 $(function(){
@@ -157,7 +275,25 @@ $(function(){
 	} else {
 		tab_empty();
 	}
+
 	bind_event();
+
+	// 窗口尺寸
+	screen_width = $(window).width();
+	screen_height = $(window).height();
+
+	// 曲谱缩放后尺寸
+	scale = (screen_width * 0.6) / current_tab.size[0][0];
+
+	tab_width = current_tab.size[0][0] * scale;
+	tab_height = current_tab.size[0][1] * scale; // 还是有偏差 1147/1159怀疑与滚动条有关
+
+	//console.log(tab_height);
+
+	// 曲谱每0.1秒滚动距离
+	step = tab_height / (current_tab.speed * 10);
+
+	//console.log(step);
 });
 </script>
 </html>
